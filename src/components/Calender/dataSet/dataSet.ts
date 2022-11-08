adbd39c493231c2880c1bcc691aadce2a1d0c9f0
KimYeongYeon <45118806+yeongyeonkim@users.noreply.github.com>
2022-11-08 17:22:19 +0900

    Create dataSet.ts

---
+++ src/components/Calender/dataSet/dataSet.ts
@@ -0,0 +1,15 @@
+import { DaySchedule } from "./DataSet.interface";
+
+export const dayScheduleData: DaySchedule[] = [
+  {
+    id: 1,
+    date: "2022-11-03",
+    title: "title"
+  },
+
+  {
+    id: 2,
+    date: "2022-11-08",
+    title: "title2"
+  }
+];
